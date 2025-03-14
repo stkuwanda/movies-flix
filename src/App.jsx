dfe205899f2d09fea1fa5e3bfb0815d7c9e91e98
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-03-14 21:00:19 +0200

    implemented an app wide Navbar component with special react-router-dom Links

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
-
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.component';
 import Favorites from './pages/Favorites.component';
 import './App.css';
-
-
+import Navbar from './components/Navbar.component';
 
 function App() {
 	return (
-		<main className='main-content'>
-			<Routes>
-				<Route path='/' element={<Home />} />
-				<Route path='/favorites' element={<Favorites />} />
-			</Routes>
-		</main>
+		<>
+			<Navbar />
+			<main className='main-content'>
+				<Routes>
+					<Route path='/' element={<Home />} />
+					<Route path='/favorites' element={<Favorites />} />
+				</Routes>
+			</main>
+		</>
 	);
 }
 
