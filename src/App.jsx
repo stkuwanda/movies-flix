592d655904bf3eae427ef01461a85b572fd5d1fc
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-03-14 20:47:01 +0200

    added basic routing using the react-router-dom npm module

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import './App.css';
-import MovieCard from './components/MovieCard.component';
+
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.component';
+import Favorites from './pages/Favorites.component';
+import './App.css';
+
+
 
 function App() {
 	return (
-		<>
-			<Home />
-		</>
+		<main className='main-content'>
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/favorites' element={<Favorites />} />
+			</Routes>
+		</main>
 	);
 }
 
