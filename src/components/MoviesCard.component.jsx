7b8d3070b92b5c6b8462b0e94d6ad466a0bf91b4
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-03-14 09:18:08 +0200

    implemented basic MoviesCard component

---
+++ src/components/MoviesCard.component.jsx
@@ -0,0 +1,24 @@
+function MoviesCard({ movie }) {
+	function handleFavoriteClick() {
+		alert('favorite clicked');
+	}
+
+	return (
+		<div className='movie-card'>
+			<div className='movie-poster'>
+				<img src={movie.url} alt={movie.title} />
+				<div className='movie-overlay'>
+					<button className='favorite-btn' onClick={handleFavoriteClick}>
+						♥
+					</button>
+				</div>
+			</div>
+			<div className='movie-info'>
+				<h3>{movie.title}</h3>
+				<p>{movie.release_date}</p>
+			</div>
+		</div>
+	);
+}
+
+export default MoviesCard;
