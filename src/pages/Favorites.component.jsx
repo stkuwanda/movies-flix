a8790eceb27ac9152d68b1b8926ad922314cf083
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-03-14 20:34:38 +0200

    implemented initial version of the Favorites component

---
+++ src/pages/Favorites.component.jsx
@@ -0,0 +1,10 @@
+function Favorites() {
+	return (
+		<div className='favorites-empty'>
+			<h2>No Favorite Movies yet</h2>
+			<p>Start adding your favorite movies and they will appear here</p>
+		</div>
+	);
+}
+
+export default Favorites;
