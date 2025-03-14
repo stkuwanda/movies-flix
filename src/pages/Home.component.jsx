000848e9c1e6dc7787177a31ff181ddb3f23a61e
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-03-14 17:22:50 +0200

    implemented search-form and form submit event handling logic

--- src/pages/Home.component.jsx
+++ src/pages/Home.component.jsx
@@ -1,18 +1,43 @@
+import { useState } from 'react';
 import MovieCard from '../components/MovieCard.component';
 
 function Home() {
+	const [searchQuery, setSearchQuery] = useState('');
+
 	const movies = [
 		{ id: 1, title: 'Lion King', release_date: '1997' },
-		{ id: 1, title: 'Aladdin', release_date: '1996' },
-		{ id: 1, title: 'Hercules', release_date: '1995' },
+		{ id: 2, title: 'Aladdin', release_date: '1996' },
+		{ id: 3, title: 'Hercules', release_date: '1995' },
 	];
+
+	function handleSearch(e) {
+		e.preventDefault();
+		alert(searchQuery);
+		setSearchQuery('');
+	}
 
 	return (
 		<div className='home'>
+			<form onSubmit={handleSearch} className='search-form'>
+				<input
+					type='text'
+					className='search-input'
+					placeholder='Search for movies...'
+					value={searchQuery}
+					onChange={(e) => setSearchQuery(e.target.value)}
+				/>
+				<button type='submit' className='search-button'>
+					Search
+				</button>
+			</form>
+
 			<div className='movies-grid'>
-				{movies.map((movie) => (
-					<MovieCard key={movie.id} movie={movie} />
-				))}
+				{movies.map(
+					(movie) =>
+						movie.title.toLowerCase().startsWith(searchQuery) && (
+							<MovieCard key={movie.id} movie={movie} />
+						)
+				)}
 			</div>
 		</div>
 	);
