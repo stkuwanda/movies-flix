8bd1a91bda2ed3f0d122e467ad619940aa17545d
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-03-15 17:55:35 +0200

    initial setup of the MovieContext so as to share state and state updating functions between separate components

---
+++ src/contexts/MovieContext.context.jsx
@@ -0,0 +1,40 @@
+import { createContext, useState, useContext, useEffect } from 'react';
+
+const MovieContext = createContext();
+export const useMovieContext = () => useContext(MovieContext);
+
+export const MovieProvider = ({ children }) => {
+	const [favorites, setFavorites] = useState([]);
+
+	useEffect(() => {
+		const storedFavorites = localStorage.getItem('favorites');
+		if (storedFavorites) setFavorites(JSON.parse(storedFavorites));
+	}, []);
+
+	useEffect(() => {
+		localStorage.setItem('favorites', JSON.stringify(favorites));
+	}, [favorites]);
+
+	const addToFavorites = (movie) => {
+		setFavorites((favorites) => [...favorites, movie]);
+	};
+
+	const removeFromFavories = (id) => {
+		setFavorites((favorites) => favorites.filter((movie) => movie.id !== id));
+	};
+
+	const isFavorite = (id) => favorites.some((movie) => movie.id === id);
+
+  const value = {
+    favorites,
+    addToFavorites,
+    removeFromFavories,
+    isFavorite
+  }
+
+	return (
+    <MovieContext.Provider value={value}>
+      {children}
+    </MovieContext.Provider>
+  );
+};
