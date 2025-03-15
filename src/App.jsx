a925d2efc88154717e2b711efa2945996f254e79
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-03-15 06:57:39 +0200

    initial setup of utility functions to handle api calls

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home.page';
 import Favorites from './pages/Favorites/Favorites.page';
-import './App.css';
 import Navbar from './components/Navbar/Navbar.component';
+import './App.css';
 
 function App() {
 	return (
---
+++ src/services/api.js
@@ -0,0 +1,26 @@
+const API_KEY = '2bd316441638278309d7ae0c062686cc';
+const BASE_URL = 'https://api.themoviedb.org/3';
+
+export async function getPopularMovies() {
+	try {
+		const response = await fetch(
+			`${BASE_URL}/movie/popular?api_key=${API_KEY}`
+		);
+		const data = await response.json();
+		return data.results;
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+export async function searchMovies(query) {
+	try {
+		const response = await fetch(
+			`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`
+		);
+		const data = await response.json();
+		return data.results;
+	} catch (error) {
+		console.error(error);
+	}
+}
